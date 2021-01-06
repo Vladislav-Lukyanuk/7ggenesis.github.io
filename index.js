f1930f2bb1606b3feb46ff66c5a3a46563a47e8a
Vladislav Lukyanuk <7ggamerussian@gmail.com>
2021-01-06 22:50:43 +0600

    save a work status

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const HOVER_COMPONENT_STATE = 'hover';
+const VISIBILITY_COMPONENT_STATE = 'visible';
 
 const COMPONENTS = {
     head: {
@@ -6,8 +7,21 @@ const COMPONENTS = {
         elements: {
             question: 'head__question'   
         }
+    },
+    fadeInBlock: {
+        block: 'fade-in-block'
     }
 }
+
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+  
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+  
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
 
 $(document).ready(() => {
     const toggleHoverState = function (prefix) {
@@ -18,4 +32,12 @@ $(document).ready(() => {
         toggleHoverState(COMPONENTS.head.elements.question),
         toggleHoverState(COMPONENTS.head.elements.question)
     );
+
+    $(window).on('resize scroll', function() {
+        $('.fade-in-block').each(function() {
+          if ($(this).isInViewport()) {
+            $(this).addClass(`${COMPONENTS.fadeInBlock.block}_${VISIBILITY_COMPONENT_STATE}`);
+          }
+        });
+    });
 });
